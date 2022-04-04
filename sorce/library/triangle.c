671b861c346638955a79130db47d28e060f2f8cd
alexichab <naumchik199@mail.ru>
2022-04-04 16:55:15 +0700

    Fixed the triangle output error

--- sorce/library/triangle.c
+++ sorce/library/triangle.c
@@ -6,7 +6,7 @@
 void TriangleInfo(struct Point points[4])
 {
     printf("\tpoints:\n");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         printf("\t\t%d: (%.3f, %.3f)\n", i + 1, points[i].x, points[i].y);
     }
 }
