b6118be7aece68814b884fb27a1bbd4aec0ca615
alexichab <naumchik199@mail.ru>
2022-04-21 16:47:40 +0700

    Changed and added functions in the triangle .c and .h file

--- sorce/library/triangle.h
+++ sorce/library/triangle.h
@@ -1,4 +1,7 @@
 #pragma once
 #include "point.h"
 
-void TriangleInfo(struct Point points[4]);
+void TriangleInfo(struct Point points[4], float perimeter, float area);
+float calculateSide(struct Point point1, struct Point point2);
+float calculateTrianglePerimeter(struct Point points[4]);
+float calculateTriangleArea(struct Point points[4]);
