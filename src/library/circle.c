3b434640dc4f6f96478014b1d8f179281b96b941
alexichab <naumchik199@mail.ru>
2022-04-28 14:39:38 +0700

    Changed the output from the CircleInfo function

--- src/library/circle.c
+++ src/library/circle.c
@@ -2,6 +2,7 @@
 #include "point.h"
 #include <math.h>
 #include <stdio.h>
+
 float calculateCirclePerimeter(float radius)//периметр круга
 {
     return 2 * M_PI * radius;
@@ -12,14 +13,13 @@ float calculateCircleArea(float radius)//площадь круга
     return M_PI * radius * radius;
 }
 
-void CircleInfo(
-        float x, float y, float radius, float perimeter, float area)//вывод дополняем площадью и периметром
+void CircleInfo(struct Circle circle)//вывод дополняем площадью и периметром
 {
-    printf("\tcenter = (%.3f %.3f)\n\tradius = %.3f\n\tperimeter = "
-           "%.3f\n\tarea = %.3f\n\n",
-           x,
-           y,
-           radius,
-           perimeter,
-           area);
+    printf("\tcenter = (%.3f %.3f)\n\tradius = %.6f\n\tperimeter = "
+           "%.6f\n\tarea = %.6f\n\n",
+           circle.center.x,
+           circle.center.y,
+           circle.radius,
+           calculateCirclePerimeter(circle.radius),
+           calculateCircleArea(circle.radius));
 }
