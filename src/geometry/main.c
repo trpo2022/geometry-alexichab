fe8b982d10d0a895701a9988716466b10262799e
alexichab <naumchik199@mail.ru>
2022-04-28 14:55:53 +0700

    Deleted the previous code that is not used.

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -60,51 +60,6 @@ int main()
     for (int i = 0; i < 3; i++) {
         getObject(i + 1, &objects[i]);
     }
-    
-//   char* input = malloc(sizeof(char) * 32);//ввод данных
-//     initInput(input);
 
-//     char delims[] = "( ,)";
-//     char* token = strtok(input, delims);
-
-//     if (strcmp(token, "circle") == 0) {//если круг
-//         printf("\n%s:\n", token);
-//         struct Point p;
-
-//         p.x = atof(strtok(NULL, delims));
-//         p.y = atof(strtok(NULL, delims));
-//         float radius = atof(strtok(NULL, delims));
-
-//         float perimeter = calculateCirclePerimeter(radius);//вычисляем периметр круга
-//         float area = calculateCircleArea(radius);//вычисляем площадь круга
-
-       
-//         CircleInfo(p.x, p.y, radius, perimeter, area);// дополняем вывод периметром и площадью
-
-//     } else if (strcmp(token, "triangle") == 0) {//если треугольник
-//         printf("\n%s:\n", token);
-//         struct Point points[4];
-
-//         for (int i = 0; i < 4; i++) {
-//             points[i].x = atof(strtok(NULL, delims));
-//             points[i].y = atof(strtok(NULL, delims));
-//         }
-
-//         if (points[3].x != points[0].x || points[3].y != points[0].y) {
-//             printf("Ошибка обработки первой/последней точки в треугольнике.\nПроверьте ввод данных.\n");
-//         }
-
-//         float perimeter = calculateTrianglePerimeter(points);//периметр треугольника
-//         float area = calculateTriangleArea(points);//площадь треугольника
-
-//         TriangleInfo(points, perimeter, area);//дополняем вывод!
-
-//     } else if (strcmp(token, "q") == 0) {
-//         return 0;
-
-//     } else {
-//         printf("Неправильный ввод данных.\n");
-//     }
-//     free(input);
   return 0;
 }
