e5031ebf2bf6725c0f553253c9372ce1c6de32a5
alexichab <naumchik199@mail.ru>
2022-04-28 14:35:03 +0700

    Created a structure for a circle in circle.h

--- src/library/circle.h
+++ src/library/circle.h
@@ -1,5 +1,11 @@
 #pragma once
+#include "point.h"
+
+struct Circle {
+    struct Point center;
+    float radius;
+};
 
 float calculateCirclePerimeter(float radius);
 float calculateCircleArea(float radius);
-void CircleInfo(float x, float y, float radius, float perimeter, float area);
+void CircleInfo(struct Circle circle);
